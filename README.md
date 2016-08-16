73458671edd0c942f39f0ce770eac4d23dc9b6c6
pacxiu <nedvedek111@gmail.com>
2016-08-16 15:50:17 +0200

    Start

---
+++ README.md
@@ -0,0 +1,3 @@
+# siteStarter
+
+Site with gulp configured with sass, autoprefixer and browsersync. Also Embedded JavaScript is enabled in this project. Just use -npm install
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var ejs = require('gulp-ejs');
+var gutil = require('gulp-util');
+
+
+
+gulp.task('ejs', function(){
+  return gulp.src('./ejs/index.ejs')
+   .pipe(ejs({}, {ext:'.html'}))
+   .pipe(gulp.dest('./'))
+});
+
+// task for sass
+gulp.task('styles', function () {
+  gulp.src('./sass/main.sass').pipe(sass())
+  // piping to autoprefixer
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./css'))
+  //.pipe(browserSync.reload({stream: true}));
+});
+
+ //task for browser sync
+ gulp.task('serve',  function () {
+   browserSync.init({
+      server: {
+        baseDir: './'
+      }
+    });
+
+gulp.watch('./sass/**', ['styles']);
+gulp.watch('./ejs/**', ['ejs']);
+
+});
+
+// default task
+gulp.task('default', ['styles', 'ejs', 'serve']);
